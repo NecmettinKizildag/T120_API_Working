5ec3556603c794af280a44025c890d48f2d076a9
Necmettin Kizildag <necmettin_kzldg@hotmail.com>
2023-11-13 18:40:00 +0100

    C08 added

--- src/test/java/test/C08_JSONArrayKullanimi.java
+++ src/test/java/test/C08_JSONArrayKullanimi.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.testng.annotations.Test;
 
@@ -35,5 +36,38 @@ public class C08_JSONArrayKullanimi {
 
         cepTel.put("type", "iPhone");
         cepTel.put("number", "0123-4567-8888");
+
+
+        JSONObject evTel = new JSONObject();
+
+        evTel.put("type", "home");
+        evTel.put("number", "0123-4567-8910");
+
+
+        JSONArray tel = new JSONArray();
+
+        tel.put(0,cepTel);
+        tel.put(1,evTel);
+
+
+
+        JSONObject adres = new JSONObject();
+
+        adres.put("streetAddress", "naist street");
+        adres.put("city", "Nara");
+        adres.put("postalCode", "630-0192");
+
+
+        JSONObject personal = new JSONObject();
+
+        personal.put("firstName", "John");
+        personal.put("lastName", "doe");
+        personal.put("age", 26);
+        personal.put("address",adres);
+        personal.put("phoneNumbers",tel);
+
+        System.out.println("personal = " + personal);
+
+
     }
 }
