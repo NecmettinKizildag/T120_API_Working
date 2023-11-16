08b91e6ab6f86e11c9ccbbb101bd47a0ddd3254a
Necmettin <necmettin_kzldg@hotmail.com>
2023-11-16 20:08:44 +0100

    C22 and testDataDummy are updated

--- src/test/java/test/C22_GET_DeSerialization.java
+++ src/test/java/test/C22_GET_DeSerialization.java
@@ -1,12 +1,19 @@
 package test;
 
-import baseUrl.jsonPlaceBaseUrl;
+import baseUrl.dummyBaseURL;
+import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
+import testDatas.TestDataJSONPlace;
+import testDatas.testDataDummy;
 
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 
-public class C22_GET_DeSerialization extends jsonPlaceBaseUrl {
+public class C22_GET_DeSerialization extends dummyBaseURL {
 
     /*
 http://dummy.restapiexample.com/api/v1/employee/3 url'ine bir GET
@@ -30,6 +37,26 @@ Response Body
 
     @Test
     public void get01(){
+        specDummy.pathParams("pp1","api","pp2","v1","pp3","employee","pp4",3);
+
+        testDataDummy testDataDummy = new testDataDummy();
+
+        HashMap<String,Object> expBody = testDataDummy.dataOlusturMAP();
+
+        Response response = given().spec(specDummy).when().get("/{pp1}/{pp2}/{pp3}/{pp4}");
+
+        HashMap<String,Object> respMAP = response.as(HashMap.class);
+
+        Assert.assertEquals(testDataDummy.okStatusKodu,response.getStatusCode());
+        Assert.assertEquals(testDataDummy.contentType,response.getContentType());
+        Assert.assertEquals(expBody.get("status"),respMAP.get("status"));
+        Assert.assertEquals(expBody.get("message"),respMAP.get("message"));
+
+        Assert.assertEquals(((Map)(expBody.get("data"))).get("id"),((Map)(respMAP.get("data"))).get("id"));
+        Assert.assertEquals(((Map)(expBody.get("data"))).get("employee_name"),((Map)(respMAP.get("data"))).get("employee_name"));
+        Assert.assertEquals(((Map)(expBody.get("data"))).get("employee_salary"),((Map)(respMAP.get("data"))).get("employee_salary"));
+        Assert.assertEquals(((Map)(expBody.get("data"))).get("employee_age"),((Map)(respMAP.get("data"))).get("employee_age"));
+        Assert.assertEquals(((Map)(expBody.get("data"))).get("profile_image"),((Map)(respMAP.get("data"))).get("profile_image"));
 
     }
 
--- src/test/java/testDatas/testDataDummy.java
+++ src/test/java/testDatas/testDataDummy.java
@@ -32,12 +32,21 @@ public class testDataDummy {
 
         HashMap<String,Object> data = new HashMap<>();
 
-        data.put("id",3);
+        data.put("id",3.0);
         data.put( "employee_name","Ashton Cox");
         data.put("employee_salary",86000.0);
         data.put("employee_age",66.0);
         data.put("profile_image","");
 
-        return data;
+
+        HashMap<String,Object> expectedBodyOlusturMAP = new HashMap<>();
+
+        expectedBodyOlusturMAP.put("status","success");
+        expectedBodyOlusturMAP.put("data",data);
+        expectedBodyOlusturMAP.put("message","Successfully! Record has been fetched.");
+
+        return expectedBodyOlusturMAP;
     }
+
+
 }
